c336bae743e3b9c18e67b786ba22b5041d68ca43
Yajun Fu <fuyajun1983cn@163.com>
2015-11-25 21:17:43 +0800

    fix org capture can't use error.

--- init.el
+++ init.el
@@ -1,9 +1,20 @@
 ;;; init.el --- Where all the magic begins
 ;;
-;; This is the first thing to get loaded.
+;; This is the first script to get loaded.
+;;
+
+;;
+;;  (setq debug-on-err t) when debugging
 ;;
 
 ;; load Org-mode from source when the ORG_HOME environment variable is set
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (when (getenv "ORG_HOME")
   (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
     (when (file-directory-p org-lisp-dir)
