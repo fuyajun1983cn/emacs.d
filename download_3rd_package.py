56d93532631212c82cc0031281b8f8b5ef0f8491
Fu Yajun <fuyajun1983cn@163.com>
2016-11-06 22:21:27 +0800

    2016年11月06日22时21分27秒

--- download_3rd_package.py
+++ download_3rd_package.py
@@ -29,3 +29,6 @@ if __name__ == "__main__":
     git_clone("https://github.com/emacsmirror/rainbow-mode.git", "rainbow-mode")
     git_clone("https://github.com/sellout/emacs-color-theme-solarized.git", "emacs-color-theme-solarized")
     git_clone("https://github.com/coldnew/linum-relative.git", "linum-relative")
+    git_clone("https://github.com/purcell/page-break-lines", "page-break-lines")
+    git_clone("https://github.com/bbatsov/projectile", "projectile")
+    git_clone("https://github.com/purcell/emacs-dashboard", "emacs-dashboard")
